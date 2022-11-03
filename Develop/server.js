cb473abb76d9abce0e211362bf90d04683a13ac5
TikoMyster <tikomyster20@gmail.com>
2022-11-02 20:05:01 -0700

    updated server.js and readme

--- Develop/server.js
+++
@@ -1,8 +0,0 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
-const path = require('path');
-
-const express = require('express');
-const app = express();
-
-const allNotes = require('./db/db.json');
